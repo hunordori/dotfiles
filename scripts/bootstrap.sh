98c98a0f0e40fa55202db618347febcf12e8d89d
Hunor Dori <hello@hunor.xyz>
2026-03-29 02:12:00 -0400

    added bash and script

---
+++ scripts/bootstrap.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+if ! command -v chezmoi >/dev/null 2>&1; then
+  if command -v brew >/dev/null 2>&1; then
+    brew install chezmoi
+  else
+    sh -c "$(curl -fsSL https://get.chezmoi.io)" -- -b "$HOME/.local/bin"
+  fi
+fi
+
+chezmoi init --apply hunordoriy
